32404c9d78565e190827ecd930c820a63891e2c0
MeGl <54992889+EndGameGl@users.noreply.github.com>
2026-06-19 19:58:14 +0300

    2nd task done

---
+++ src/ConcurrentIncrementer/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConcurrentIncrementer;
+
+Parallel.For(
+    fromInclusive: 1,
+    toExclusive: 201,
+    body: (i) =>
+    {
+        ServerImplementation.AddToCount(i);
+        var value = ServerImplementation.GetCount();
+    }
+);
+
+Console.WriteLine(ServerImplementation.GetCount());
---
+++ src/ConcurrentIncrementer/ServerImplementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrentIncrementer;
+
+internal static class ServerImplementation
+{
+    private static int _counter = 0;
+    private static readonly ReaderWriterLockSlim _rwls = new();
+
+    public static void AddToCount(int value)
+    {
+        try
+        {
+            _rwls.EnterWriteLock();
+            _counter += value;
+        }
+        finally
+        {
+            _rwls.ExitWriteLock();
+        }
+    }
+
+    public static int GetCount()
+    {
+        try
+        {
+            _rwls.EnterReadLock();
+            return _counter;
+        }
+        finally
+        {
+            _rwls.ExitReadLock();
+        }
+    }
+}
